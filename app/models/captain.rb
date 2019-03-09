0922bd7a5f6ef76c47267bff35b3ef84fe7c87ad
Virginia Dooley <vcdooley@gmail.com>
2019-03-09 13:42:26 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,20 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
+  end
+
+  def self.talented_seafarers
+    # returns captains of motorboats and sailboats (FAILED - 4)
+  end
+
+  def self.non_sailors
+    # returns people who are not captains of sailboats (FAILED - 5)
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,12 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+      # returns all classifications (FAILED - 6)
+  end
+
+  def self.longest
+      # returns the classifications for the longest boat (FAILED - 7)
+  end 
 end
